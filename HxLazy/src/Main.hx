46627e1b636e938a14751ca2b37fcd61a7ac91cf
Jonas Nyström <jonasnys@gmail.com>
2014-06-15 15:52:38 +0200

    Now works with non-block method returns

--- HxLazy/src/Main.hx
+++ HxLazy/src/Main.hx
@@ -10,7 +10,12 @@ class Main
 		trace(t.helloWorld);
 		trace(t.helloWorld);
 		trace(t.getTestfloat());
-		
+		trace(t.wihtoutBlock);
+		trace(t.wihtoutBlock);
+		trace(t.wihtoutBlock2);
+		trace(t.wihtoutBlock2);
+		trace(t.getWithoutBlock3());
+		trace(t.getWithoutBlock3());
 	}
 }
  
@@ -35,6 +40,26 @@ class TestLazy implements Lazy
 	{
 		trace('Init testFloat - should only be traced once');
 		return 1.2 + 3.4;
+	}
+	
+	@lazy public function wihtoutBlock():String return retString();
+	@lazy public function wihtoutBlock2():{x:Float} return retAnon();
+	@lazyGet public function withoutBlock3():Int return retInt();
+	
+	private function retInt()
+	{
+		trace('Redundant method: retInt() - should only be called once');
+		return 432;
+	}
+	private function retAnon()
+	{
+		trace('Redundant method: retAnon() - should only be called once');
+		return {x: 1.234};
+	}
+	private function retString()
+	{
+		trace('Redundant method: retString() - should only be called once');
+		return 'Hello from return method!';
 	}
 	
 	
--- HxLazy/src/hxlazy/LazyBuilder.hx
+++ HxLazy/src/hxlazy/LazyBuilder.hx
@@ -9,20 +9,23 @@ using Lambda;
 
  class LazyBuilder 
 {
-	
 	static var LAZY = 'lazy';
-	static var LAZY_GET= 'lazyGet';
+	static var LAZY_GET = 'lazyGet';
+	static var LAZY_PRIVATE_PREFIX = '__lazy';
 	
 	public static function build():Array<Field>
 	{
 		var originalFields = Context.getBuildFields();
 		var resultFields:Array<Field> = [];
+		
 		var lazyFields = originalFields.filter(function(f) return f.meta.exists(function(m) return m.name == LAZY));
 		var lazyGetFields = originalFields.filter(function(f) return f.meta.exists(function(m) return m.name == LAZY_GET));
 		var nonLazyFields = originalFields.filter(function(f) return ! lazyFields.has(f) && ! lazyGetFields.has(f));
+		
 		nonLazyFields.iter(function(f) resultFields.push(f));
 		lazyFields.iter(function(f) resultFields = resultFields.concat(createLazyFields(f, LAZY)));
 		lazyGetFields.iter(function(f) resultFields = resultFields.concat(createLazyFields(f, LAZY_GET)));
+		
 		return resultFields;
 	}
 
@@ -30,7 +33,8 @@ using Lambda;
 	{
 		var lazyfields:Array<Field> = [];
 		var fieldName = f.name;
-		var fieldType = switch f.kind {
+		var fieldType = switch f.kind 
+		{
 			case FFun(f2): f2.ret;
 			case _: 
 				Context.warning("The 'lazy' meta...", f.pos);
@@ -43,7 +47,7 @@ using Lambda;
 			case _: return [f];
 		}
 		
-		var privateFieldName = '__lazy' + fieldName;
+		var privateFieldName = LAZY_PRIVATE_PREFIX + fieldName;
 		var nullField = TPath( { name:'Null', pack:[], params:[TPType(fieldType)] } );
 		lazyfields.push({
 			name: privateFieldName,
@@ -83,28 +87,30 @@ using Lambda;
 		});
 
 		return lazyfields;
-	}
-   
+	}   
+	
 	static private function createLazyGetter(privateFieldName:String, functionBody:Expr) : Function
 	{
-		 var replaceReturnValue:Expr -> Void;
-		 replaceReturnValue = function(e:Expr) {
-			 switch e {
-				 case macro return $retval: 
+		var replaceReturnValue:Expr -> Void;
+		replaceReturnValue = function(e:Expr) {
+			switch e {
+				case macro return $retval:
 					var change = macro return $i{privateFieldName} = $retval;
 					e.expr = change.expr;
 				case _: e.iter(replaceReturnValue);
-			 }
-		 }
+			}
+		}
 
-		 functionBody.iter(replaceReturnValue);
+		functionBody.iter(replaceReturnValue);
 
-		 switch functionBody.expr {
-			 case EBlock(exprs): 
-				 exprs.unshift(macro if ( $i { privateFieldName } != null) return $i { privateFieldName } );
-			// case OBS hantera fall där metoden saknar block!
-			case _: 
-		 }
+		switch functionBody.expr {
+			case EBlock(exprs):
+				exprs.unshift(macro if ( $i { privateFieldName } != null) return $i { privateFieldName } );
+			case EReturn(expr):
+				var xpr  = macro if ( $i { privateFieldName } != null) return $i { privateFieldName } else return $i { privateFieldName } = $expr ;
+				functionBody.expr = EReturn(xpr); 
+			case _:
+		}
 
 		var result = {
 			ret: null,
@@ -112,7 +118,7 @@ using Lambda;
 			expr: functionBody,
 			args: []
 		}
-		   
-		return result;  
-	}
+
+		return result;
+	}	
 }
